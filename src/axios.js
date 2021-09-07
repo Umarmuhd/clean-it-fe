c381ace4cb06f77c6d6b3ad8073c637a7bf77839
umarmuhd <umarfarouqft@gmail.com>
2021-09-07 18:56:35 +0100

    added live url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /*Base URL Makes request to the Server API*/
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://murmuring-wave-94302.herokuapp.com//api/v1/",
 });
 
 export default instance;
