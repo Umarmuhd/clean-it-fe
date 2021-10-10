5e15aab5cd951c3a227cfffa2f5f69fabe067962
umarmuhd <umarfarouqft@gmail.com>
2021-10-10 16:56:27 +0100

    change api

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /*Base URL Makes request to the Server API*/
 const instance = axios.create({
-  baseURL: "https://murmuring-wave-94302.herokuapp.com//api/v1/",
+  baseURL: "https://murmuring-wave-94302.herokuapp.com/api/v1",
 });
 
 export default instance;
